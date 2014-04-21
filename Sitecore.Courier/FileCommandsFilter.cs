6781da51a39c0b54f2ccb144043d958350fe603d
Alexander Doroshenko <adoprog@gmail.com>
2014-04-21 19:03:41 +0300

    Fixed  #1, disabled file commands (to be fixed in other way soon)

---
+++ Sitecore.Courier/FileCommandsFilter.cs
@@ -0,0 +1,37 @@
+﻿namespace Sitecore.Courier
+{
+  using Sitecore.Update.Commands;
+  using Sitecore.Update.Interfaces;
+
+  /// <summary>
+  /// The language filter.
+  /// </summary>
+  public class FileCommandsFilter : ICommandFilter
+  {
+    /// <summary>
+    /// Filters the command. 
+    /// </summary>
+    /// <param name="command">The command.</param>
+    /// <returns>The filtered command.</returns>
+    public ICommand FilterCommand(ICommand command)
+    {
+      if ((command is ChangeFileCommand) || (command is AddFileCommand) || (command is AddFolderCommand))
+      {
+        return null;
+      }
+
+      return command;
+    }
+
+    /// <summary>
+    /// Clones this instance.
+    /// </summary>
+    /// <returns>
+    /// The I command filter.
+    /// </returns>
+    public ICommandFilter Clone()
+    {
+      return new FileCommandsFilter();
+    }
+  }
+}
