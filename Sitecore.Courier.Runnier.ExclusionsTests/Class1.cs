892334b518b30c649362ad338588632ad249a47d
First Last <christopher.gutmanis@aurora.org>
2015-09-09 17:09:30 -0500

    moar tests.

--- Sitecore.Courier.Runnier.ExclusionsTests/Class1.cs
+++ Sitecore.Courier.Runnier.ExclusionsTests/Class1.cs
@@ -59,5 +59,64 @@ namespace Sitecore.Courier.Runner.ExclusionsTests
         {
             Assert.Throws<FileNotFoundException>(() => ExclusionValidator.HasValidExclusions("myconfig", "invalidfile.xml"));
         }
+
+        [Test]
+        public void HasValidBuildConfigurationAndFileTest()
+        {
+            var expected = true;
+            var path = Environment.CurrentDirectory;
+            var xmlPath = string.Concat(path.Substring(0, path.IndexOf("bin")), "sample.xml");
+            var actual = ExclusionValidator.HasValidExclusions("myconfig", xmlPath);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void HasCorrectNumberOfExcludedItemsTest()
+        {
+            var expected = 3;
+            var path = Environment.CurrentDirectory;
+            var xmlPath = string.Concat(path.Substring(0, path.IndexOf("bin")), "sample.xml");
+            var exclusions = ExclusionReader.GetExcludedItems(xmlPath, "Test");
+            Assert.AreEqual(expected, exclusions.Count);            
+        }
+
+        [Test]
+        public void ReturnsCorrectExcludedItemsTest()
+        {
+            var expected = new string[]
+            {
+                "sitecore\\content\\mysite\\excludefrombuild1.item", 
+                "sitecore\\content\\mysite\\excludefrombuild2.item",
+                "sitecore\\content\\mysite\\excludefrombuild3.item"
+            };
+
+            var path = Environment.CurrentDirectory;
+            var xmlPath = string.Concat(path.Substring(0, path.IndexOf("bin")), "sample.xml");
+            var exclusions = ExclusionReader.GetExcludedItems(xmlPath, "Test");
+            Assert.AreEqual(expected, exclusions);  
+        }
+
+        [Test]
+        public void DoesNotReturnIncorrectExcludedItemsTest()
+        {
+            var path = Environment.CurrentDirectory;
+            var xmlPath = string.Concat(path.Substring(0, path.IndexOf("bin")), "sample.xml");
+            var exclusions = ExclusionReader.GetExcludedItems(xmlPath, "Test");
+            Assert.False(exclusions.Contains("sitecore\\content\\mysite\\donotexcludefrombuild1.item"));
+        }
+
+
+        [Test]
+        public void DoesNotReturnForConfigurationWithoutExclusionsTest()
+        {
+            var path = Environment.CurrentDirectory;
+            var xmlPath = string.Concat(path.Substring(0, path.IndexOf("bin")), "sample.xml");
+            var configuration = "NO_RESULTS_CONFIGURATION";
+
+            var actual = ExclusionValidator.HasValidExclusions(configuration, xmlPath);
+            Assert.True(actual);
+            var exclusions = ExclusionReader.GetExcludedItems(xmlPath, configuration);
+            Assert.False(exclusions.Any());
+        }
     }
 }
