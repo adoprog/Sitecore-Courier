c15ece6ed5c7020b8ab7e4f4e20d35ebfb1234ef
Alexander Doroshenko <ado@sitecore.net>
2017-10-27 12:48:49 +0300

    Updated Choco script to download zip from GitHib releases

--- Choco/tools/chocolateyInstall.ps1
+++ Choco/tools/chocolateyInstall.ps1
@@ -1,5 +1,13 @@
-﻿$srm = (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'srm.exe')
-Write-Output $MyInvocation.MyCommand.Definition
-$ext =  (Join-Path $(Split-Path -parent $MyInvocation.MyCommand.Definition) 'Sitecore.Courier.ShellExtensions.dll')
+﻿$name = "sitecore-courier"
+$url = "https://github.com/adoprog/Sitecore-Courier/releases/download/1.2/Sitecore.Courier.Runner.zip"
+$unzipLocation = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+Write-Output "Unzip location: $unzipLocation"
+
+Install-ChocolateyZipPackage $name $url $unzipLocation
+ 
+$srm = (Join-Path $(Split-Path -parent $unzipLocation) 'tools\srm.exe')
+$ext =  (Join-Path $(Split-Path -parent $unzipLocation) 'tools\Sitecore.Courier.ShellExtensions.dll')
+Write-Output $srm
 Write-Output $ext
 & $srm install $ext -codebase
+ 
