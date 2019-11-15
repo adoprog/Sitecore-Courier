e1241aa35f95951bc772e9ae867f287ac7ee5d24
Alexander Doroshenko <ado@sitecore.net>
2019-11-15 14:33:40 +0200

    #40 Refactoring, merged diff generators

--- Sitecore.Courier/DIffGenerator.cs
+++ Sitecore.Courier/DIffGenerator.cs
@@ -1,167 +1,145 @@
-﻿using System.Linq;
-using Sitecore.Courier.Iterators;
-using Sitecore.Update.Commands;
-using System.Collections.Generic;
+﻿using Sitecore.Courier.Iterators;
 using Sitecore.Update;
 using Sitecore.Update.Configuration;
 using Sitecore.Update.Data;
+using Sitecore.Update.Data.Items;
 using Sitecore.Update.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Sitecore.Courier
 {
-  /// <summary>
-  /// Defines the Diff generator class.
-  /// </summary>
-  public class DiffGenerator
-  {
-    public static bool IncludeSecurity { get; set; }
-    public static string Version { get; set; }
-
-    public static List<ICommand> GetDiffCommands(string sourcePath, string targetPath, bool includeSecurity, string version, CollisionBehavior collisionBehavior = CollisionBehavior.Undefined)
-    {
-      IncludeSecurity = includeSecurity;
-      Version = version;
-
-      var sourceManager = Factory.Instance.GetSourceDataManager();
-      var targetManager = Factory.Instance.GetTargetDataManager();
-
-      sourceManager.SerializationPath = sourcePath;
-      targetManager.SerializationPath = targetPath;
-
-      IDataIterator sourceDataIterator = sourceManager.ItemIterator ?? new EmptyIterator();
-      IDataIterator targetDataIterator = targetManager.ItemIterator;
-
-      var engine = new DataEngine();
-      var targetItemIDs = GetItemIDs(targetPath);
-      var commands = GetDiffCommands(targetItemIDs, collisionBehavior, sourceDataIterator, targetDataIterator, engine);
-      engine.ProcessCommands(ref commands);
-      return commands;
-    }
-
-    public static List<ICommand> GetDiffCommands(HashSet<string> targetItemIDs, CollisionBehavior collisionBehavior,
-      IDataIterator sourceDataIterator, IDataIterator targetDataIterator, DataEngine engine)
-    {
-      var commands = new List<ICommand>();
-      commands.AddRange(GenerateDiff(sourceDataIterator, targetDataIterator));
-
-      //if an item is found to be deleted AND added, we can be sure it's a move
-      var deleteCommands = commands.OfType<DeleteItemCommand>();
-      var shouldBeUpdateCommands =
-        commands.OfType<AddItemCommand>()
-          .Select(a => new
-          {
-            Added = a,
-            Deleted = deleteCommands.FirstOrDefault(d => d.ItemID == a.ItemID)
-          }).Where(u => u.Deleted != null).ToList();
-      foreach (var command in shouldBeUpdateCommands)
-      {
-        commands.AddRange(command.Deleted.GenerateUpdateCommand(command.Added));
-        commands.Remove(command.Added);
-        commands.Remove(command.Deleted);
-
-        //now, this one is an assumption, but would go wrong without the assumption anyway: this assumption is in fact safer
-        //if the itempath of a delete command starts with this delete command, it will be moved along to the new node, not deleted, just leave it alone
-        //but we will skip items which are not in the target folder
-        commands.RemoveAll(c =>
-          c is DeleteItemCommand && 
-          ((DeleteItemCommand) c).ItemPath.StartsWith(command.Deleted.ItemPath) &&
-          targetItemIDs.Contains(((DeleteItemCommand) c).ItemID));
-      }
-
-      commands.ForEach(_ => _.CollisionBehavior = collisionBehavior);
-      return commands;
-    }
-
-    private static HashSet<string> GetItemIDs(string path)
-    {
-      var itemIDs = new HashSet<string>();
-      if (string.IsNullOrEmpty(path))
-      {
-        return itemIDs;
-      }
-
-      var dataManager = Factory.Instance.GetTargetDataManager();
-      dataManager.SerializationPath = path;
-      IDataIterator dataIterator = dataManager.ItemIterator;
-
-      var item = dataIterator.Next();
-      while (item != null && item is QuickContentDataItem)
-      {
-        itemIDs.Add((item as QuickContentDataItem).ItemID);
-        item = dataIterator.Next();
-      }
-
-      return itemIDs;
-    }
-
     /// <summary>
-    /// Generates the diff.
+    /// Defines the Diff generator class.
     /// </summary>
-    /// <param name="sourceIterator">The source iterator.</param>
-    /// <param name="targetIterator">The target iterator.</param>
-    /// <returns>
-    /// The diff.
-    /// </returns>
-    protected static IList<ICommand> GenerateDiff(IDataIterator sourceIterator, IDataIterator targetIterator)
+    public class DiffGenerator
     {
-      List<ICommand> commands = new List<ICommand>();
-      IDataItem sourceDataItem = sourceIterator.Next();
-      IDataItem targetDataItem = targetIterator.Next();
+        public static bool IncludeSecurity { get; set; }
+        public static string Version { get; set; }
 
-      while (sourceDataItem != null || targetDataItem != null)
-      {
-        int compareResult = Compare(sourceDataItem, targetDataItem);
-        commands.AddRange((sourceDataItem ?? targetDataItem).GenerateDiff(sourceDataItem, targetDataItem, compareResult));
-        if (compareResult < 0)
+        public static List<ICommand> GetDiffCommands(string sourcePath, string targetPath, bool includeSecurity, string version, CollisionBehavior collisionBehavior = CollisionBehavior.Undefined)
         {
-          sourceDataItem = sourceIterator.Next();
+            IncludeSecurity = includeSecurity;
+            Version = version;
+
+            var sourceManager = Factory.Instance.GetSourceDataManager();
+            var targetManager = Factory.Instance.GetTargetDataManager();
+
+            sourceManager.SerializationPath = sourcePath;
+            targetManager.SerializationPath = targetPath;
+
+            IDataIterator sourceDataIterator = sourceManager.ItemIterator ?? new EmptyIterator();
+            IDataIterator targetDataIterator = targetManager.ItemIterator;
+            
+            var commands = GetCommands(sourceDataIterator, targetDataIterator);
+            var engine = new DataEngine();
+            engine.ProcessCommands(ref commands);
+            return commands;
         }
-        else
+
+        public static List<ICommand> GetCommands(IDataIterator sourceDataIterator, IDataIterator targetDataIterator)
         {
-          if (compareResult > 0)
-          {
-            targetDataItem = targetIterator.Next();
-          }
-          else
-          {
-            if (compareResult == 0)
+            var source = Map(sourceDataIterator);
+            var target = Map(targetDataIterator);
+
+            var commands = GetCommands(source, target);
+            return commands;
+        }
+
+        /// <summary>
+        /// Goes through source and target and creates add/update/delete commands as needed
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        protected static List<ICommand> GetCommands(Dictionary<string, IDataItem> source, Dictionary<string, IDataItem> target)
+        {
+            var commands = new List<ICommand>();
+            //Iterate through all source items and match them in target. If there's a match, update it and remove from target. Else delete
+            foreach (var old in source)
             {
-              targetDataItem = targetIterator.Next();
-              sourceDataItem = sourceIterator.Next();
+                if (target.ContainsKey(old.Key))
+                {
+                    commands.AddRange(old.Value.GenerateUpdateCommand(target[old.Key]));
+                    target.Remove(old.Key);
+                }
+                else
+                {
+                    commands.AddRange(old.Value.GenerateDeleteCommand());
+                }
             }
-          }
+
+            //The ones still left in target will be the ones that did not compare to any item in source, create add commands
+            commands.AddRange(target.Values.SelectMany(t => t.GenerateAddCommand()));
+
+            return commands;
         }
-      }
-    
-      return commands;
+
+        /// <summary>
+        /// Maps an iterator into a Dictionary with unique keys for each item.
+        /// </summary>
+        /// <param name="iterator"></param>
+        /// <returns></returns>
+        protected static Dictionary<string, IDataItem> Map(IDataIterator iterator)
+        {
+            var dict = new Dictionary<string, IDataItem>();
+            while (true)
+            {
+                var item = iterator.Next();
+                if (item != null)
+                {
+                    var key = CreateUniqueKey(item);
+                    try
+                    {
+                        dict.Add(key, item);
+                    }
+                    catch (ArgumentException)
+                    {
+                        //This should not happen unless the dataset is corrupted, which would mean it would not give a proper result
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Two items got the same unique key. This suggests invalid data. Check {GetItemPath(item)} vs {GetItemPath(dict[key])}");
+                        Console.ResetColor();
+                    }
+                }
+                else
+                {
+                    return dict;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Creates a unique key for an item (a file is the path, an item is the database and ID)
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        protected static string CreateUniqueKey(IDataItem item)
+        {
+            if (item is ContentDataItem)
+            {
+                //Unique key is database and item id
+                var content = item as ContentDataItem;
+                return $"content::{content.DatabaseName}::{content.ItemID}";
+            }
+            else if (item is FileSystemDataItem)
+            {
+                //The path should always be unique for files on windows
+                var file = item as FileSystemDataItem;
+                return $"file::{file.RelatedPath}";
+            }
+            //This should not happen, if it does the throw helps us spot it!
+            throw new Exception($"Unhandled DataItem type {GetItemPath(item)}");
+        }
+
+        /// <summary>
+        /// Only used for improved exception messages (even though it should give the same value as file.HashCode above)
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        protected static string GetItemPath(IDataItem item)
+        {
+            return string.Join("\\", new[] { item.RootPath, item.RelatedPath }.Where(s => !string.IsNullOrWhiteSpace(s)));
+        }
     }
-
-    /// <summary>
-    /// Compares the specified source item.
-    /// </summary>
-    /// <param name="sourceItem">The source item.</param>
-    /// <param name="targetItem">The target item.</param>
-    /// <returns>
-    /// The int32.
-    /// </returns>
-    protected static int Compare(IDataItem sourceItem, IDataItem targetItem)
-    {
-        if (sourceItem == null && targetItem == null)
-        {
-            return 0;
-        }
-
-        if (sourceItem == null)
-        {
-            return 1;
-        }
-
-        if (targetItem == null)
-        {
-            return -1;
-        }
-
-        return sourceItem.CompareTo(targetItem);
-    }
-  }
 }
